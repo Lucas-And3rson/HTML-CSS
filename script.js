b65a1bbd4587ffcfdb46cfe738dd75046e5eddf5
Lucas-And3rson <lucasestuda09@gmail.com>
2022-11-10 15:37:09 -0300

    Código atualizado
    Algumas modificações no comando

---
+++ script.js
@@ -0,0 +1,11 @@
+function w3_open() {
+    var x = document.getElementById("menuLateral");
+    x.style.width = "10%";
+    x.style.fontSize = "12px";
+    x.style.paddingTop = "5%";
+    x.style.display = "block";
+  }
+  function w3_close() {
+    document.getElementById("menuLateral").style.display = "none";
+  }
+  
